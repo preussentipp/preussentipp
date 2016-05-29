0fd7b428138194abf30c50bb33c8cca7afed2a42
Tony <th@th.de>
2016-05-29 17:55:19 +0200

    Some files (test)

---
+++ src/main/java/de/hoffmato/preussentipp/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package de.hoffmato.preussentipp.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+ 
+}
--- src/main/java/de/hoffmato/preussentipp/controller/IndexController.java
+++ src/main/java/de/hoffmato/preussentipp/controller/IndexController.java
@@ -1,17 +1,58 @@
 package de.hoffmato.preussentipp.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("recipient", "World");
         //return "index.html";
-        return "keks.html";
+        return "index.html";
     }
+    
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage(ModelMap model) {
+        model.addAttribute("user", getPrincipal());
+        return "admin.html";
+    }
+ 
+    @RequestMapping(value = "/db", method = RequestMethod.GET)
+    public String dbaPage(ModelMap model) {
+        model.addAttribute("user", getPrincipal());
+        return "dba.html";
+    }
+    
+    @RequestMapping(value = "/snoop", method = RequestMethod.GET)
+    public String snoop(Model model) {
+        model.addAttribute("recipient", "World");
+        //return "index.html";
+        System.out.println("BLLLLLL");
+        return "snoop.jsp";
+    }
+    
+    @RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
+    public String accesDenied(Model model) {
+        return "access-denied.html";
+    }
+    
+    private String getPrincipal(){
+        String userName = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+ 
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails)principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+        return userName;
+    }
+    
 }
